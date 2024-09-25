e959c62d8adbd9ee9fe46001d6c94cb0bc459944
mario345th <175557234+mario345th@users.noreply.github.com>
2024-09-25 14:00:40 -0600

    En prueba

---
+++ src/controlador/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import modelo.OperacionesMatematicas;
+
+/**
+ *
+ * @author M345
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        OperacionesMatematicas o = new OperacionesMatematicas ();
+        System.out.println(o.factorial(5));
+    }
+
+}
